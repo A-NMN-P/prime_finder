348b2bb65251f9fa3f844fe3bb8bc585f341df20
agnoboros <108662645+Agnoboros@users.noreply.github.com>
2023-09-30 21:24:42 +0300

    Update prime_function_test.py

--- prime_function_test.py
+++ prime_function_test.py
@@ -7,10 +7,15 @@ Created on Tue Aug  1 19:34:39 2023
 
 from prime_finder import Prime_Finder, isPrime
 
+# Find the prime numbers in a specific range:
 p, unp, div = Prime_Finder(initilizer = 4, limit = 10, prime_list=True, unprime_list=True, division_list=True, step = 3)
 
-print("\nAsal sayılar: \n", p)
-print("\nAsal olmayan sayılar: \n", unp)
-print("\nBölenler:\n", div)
+print("\nPrime Numbers: \n", p)
+print("\nNone Prime Numbers: \n", unp)
+print("\Dividers:\n", div)
 
+# Check the number is prime or not:
+primeness, division_list = isPrime(10,True)
+print("\nGiven number is prime or not: ", primeness)
+print("\nThe division list: ", division_list)
 
